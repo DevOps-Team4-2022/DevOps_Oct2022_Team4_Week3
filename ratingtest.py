48d39789b93bd628e2ec06fd4050a1ab91780601
vanweiting <your email>
2022-11-13 22:52:20 +0800

    ratingtest.py still got error

--- ratingtest.py
+++ ratingtest.py
@@ -1,2 +1,17 @@
-Python 3.10.8 (tags/v3.10.8:aaaf517, Oct 11 2022, 16:50:30) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+import time
+
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("https://www.nlb.gov.sg/main/home")
+driver.maximize_window()
+
+# click to view the ratings available for selection
+see_rating_choices = '//*[@id="wog--sentiments-content"]/wog-tabbed-widget/div/div/p'
+driver.find_element_by_xpath(see_rating_choices).click()
+
+# click the 6th rating that represents "Very Stisfied"
+rating_choice = '//*[@id="wog--sentiments-content"]/wog-rating/div/div/div[2]/wog-emoji-rating-scale-field/div/div[1]/div[6]/button/svg/path[2]'
+driver.find_element_by_xpath(rating_choice).click()
+
+time.sleep(5)
