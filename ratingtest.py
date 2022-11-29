30fe2a9b3273b2484ff4eae2925c00165ec2f416
vanweiting <your email>
2022-11-29 22:03:38 +0800

    still got errors

--- ratingtest.py
+++ ratingtest.py
@@ -1,6 +1,7 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 chromedriver_location = "/Users/Downloads/chromedriver"
 driver = webdriver.Chrome(chromedriver_location)
@@ -8,11 +9,15 @@ driver.get("https://www.nlb.gov.sg/main/home")
 driver.maximize_window()
 
 # click to view the ratings available for selection
-see_rating_choices = '//*[@id="wog--sentiments-content"]/wog-tabbed-widget/div/div/p'
-driver.find_element_by_xpath(see_rating_choices).click()
+driver.find_element(By.CLASS_NAME, '//*[@id="wog--sentiments-content"]/wog-tabbed-widget/div/div').click()
 
 # click the 6th rating that represents "Very Stisfied"
-rating_choice = '//*[@id="wog--sentiments-content"]/wog-rating/div/div/div[2]/wog-emoji-rating-scale-field/div/div[1]/div[6]/button/svg/path[2]'
+rating_choice = '//*[@id="wog--sentiments-content"]/wog-rating/div/div/div[2]/wog-emoji-rating-scale-field/div/div[1]/div[6]/button/svg/path[1]'
 driver.find_element_by_xpath(rating_choice).click()
+time.sleep(4)
+
+# close "Tell us more" pop-up
+popUp = '//*[@id="wog--sentiments-content"]/wog-modern-feedback/div/wog-close-button/button/svg/line[1]'
+driver.find_element_by_xpath(popUp).click()
 
 time.sleep(5)
